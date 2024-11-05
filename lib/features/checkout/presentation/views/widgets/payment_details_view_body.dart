6f40de75cff2f10d3f3c8e9fcb44076fc871cf2f
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-05 04:13:31 +0200

    in active payment method

--- lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,44 +1,14 @@
+import 'package:checkout_payment_app/features/checkout/presentation/views/widgets/payment_method_item.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
   const PaymentDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 100,
-          height: 60,
-          decoration: ShapeDecoration(
-            shape: RoundedRectangleBorder(
-              side: const BorderSide(
-                width: 1.5,
-                color: Color(0xff43A835),
-              ),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            shadows: const [
-              BoxShadow(
-                  color: Color(0xff43A835),
-                  blurRadius: 4,
-                  offset: Offset(0, 0),
-                  spreadRadius: 0),
-            ],
-          ),
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16), color: Colors.white),
-            child: Center(
-              child: SvgPicture.asset(
-                'assets/images/credit_card.svg',
-                height: 24,
-              ),
-            ),
-          ),
-        )
-      ],
+    return const Column(
+      children: [PaymentMethodItem(isActive: false)],
     );
   }
 }
+
---
+++ lib/features/checkout/presentation/views/widgets/payment_method_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class PaymentMethodItem extends StatelessWidget {
+  const PaymentMethodItem({super.key, required this.isActive});
+
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 600),
+      width: 100,
+      height: 60,
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            width: 1.5,
+            color: isActive ? const Color(0xff43A835) : Colors.grey,
+          ),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        shadows: [
+          BoxShadow(
+            color: isActive ? const Color(0xff43A835) : Colors.white,
+            blurRadius: 4,
+            offset: const Offset(0, 0),
+            spreadRadius: 0,
+          )
+        ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.white,
+        ),
+        child: Center(
+          child: SvgPicture.asset(
+            'assets/images/credit_card.svg',
+            height: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
