a050731d9b666480e24ea43d49603b3a09f39a53
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-05 19:48:17 +0200

    finish thank you card

--- lib/features/checkout/presentation/views/widgets/thank_you_card.dart
+++ lib/features/checkout/presentation/views/widgets/thank_you_card.dart
@@ -3,6 +3,7 @@ import 'package:checkout_payment_app/features/checkout/presentation/views/widget
 import 'package:checkout_payment_app/features/checkout/presentation/views/widgets/payment_item_info.dart';
 import 'package:checkout_payment_app/features/checkout/presentation/views/widgets/total_price.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ThankYouCard extends StatelessWidget {
   const ThankYouCard({super.key});
@@ -12,7 +13,7 @@ class ThankYouCard extends StatelessWidget {
     return Container(
       width: double.infinity,
       decoration: ShapeDecoration(
-        color: Color(0xffD9D9D9),
+        color: const Color(0xffD9D9D9),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
@@ -21,7 +22,7 @@ class ThankYouCard extends StatelessWidget {
         padding: const EdgeInsets.only(top: 25 + 18, left: 22, right: 22),
         child: Column(
           children: [
-            Text(
+            const Text(
               'Thank you!',
               style: Styles.style25,
               textAlign: TextAlign.center,
@@ -31,23 +32,57 @@ class ThankYouCard extends StatelessWidget {
               style: Styles.style20,
               textAlign: TextAlign.center,
             ),
-            SizedBox(height: 42),
-            PaymentItemInfo(title: 'Date', value: '01/24/2023'),
-            SizedBox(height: 20),
-            PaymentItemInfo(title: 'Time', value: '10:15 AM'),
-            SizedBox(height: 20),
-            PaymentItemInfo(title: 'To', value: 'To'),
-            Divider(
+            const SizedBox(height: 42),
+            const PaymentItemInfo(title: 'Date', value: '01/24/2023'),
+            const SizedBox(height: 20),
+            const PaymentItemInfo(title: 'Time', value: '10:15 AM'),
+            const SizedBox(height: 20),
+            const PaymentItemInfo(title: 'To', value: 'To'),
+            const Divider(
               thickness: 2,
               height: 60,
             ),
-            TotalPrice(title: 'Total', value: r'$50.97'),
-            SizedBox(height: 30),
-            CardInfoWidget(),
+            const TotalPrice(title: 'Total', value: r'$50.97'),
+            const SizedBox(height: 30),
+            const CardInfoWidget(),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(
+                  FontAwesomeIcons.barcode,
+                  size: 64,
+                ),
+                Container(
+                  width: 113,
+                  height: 58,
+                  decoration: ShapeDecoration(
+                    color: const Color(0xffD9D9D9),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      side: const BorderSide(
+                        width: 1.5,
+                        color: Color(0xff34A853),
+                      ),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Paid',
+                      style: Styles.style24.copyWith(
+                        color: const Color(0xff34A853),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: ((MediaQuery.sizeOf(context).height * 0.2 + 20) / 2) - 29 ,
+            ),
           ],
         ),
       ),
     );
   }
 }
-
