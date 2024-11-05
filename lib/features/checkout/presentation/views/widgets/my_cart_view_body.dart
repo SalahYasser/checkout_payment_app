f118d43ef4b9f4337d580b1a3cf89a6fe3baacf7
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-05 02:41:14 +0200

    create order info item

--- lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:checkout_payment_app/features/checkout/presentation/views/widgets/order_info_item.dart';
 import 'package:flutter/material.dart';
 
 class MyCartViewBody extends StatelessWidget {
@@ -5,11 +6,21 @@ class MyCartViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 18),
-        Image.asset('assets/images/basket.png'),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(height: 18),
+          Image.asset('assets/images/basket.png'),
+          const SizedBox(height: 25),
+          const OrderInfoItem(title: 'Order Subtotal', value: r'$42.97'),
+          const SizedBox(height: 3),
+          const OrderInfoItem(title: 'Discount', value: r'$0'),
+          const SizedBox(height: 3),
+          const OrderInfoItem(title: 'Shipping', value: r'$8'),
+        ],
+      ),
     );
   }
 }
+
