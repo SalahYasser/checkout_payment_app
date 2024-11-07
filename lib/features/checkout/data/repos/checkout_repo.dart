b973753892d04b51c892c350f4067cfc0713934c
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-07 05:22:55 +0200

    implement make Payment method for checkout repo

--- lib/features/checkout/data/repos/checkout_repo.dart
+++ lib/features/checkout/data/repos/checkout_repo.dart
@@ -1,9 +1,24 @@
 import 'package:checkout_payment_app/core/errors/failures.dart';
+import 'package:checkout_payment_app/core/utils/stripe_service.dart';
 import 'package:checkout_payment_app/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 abstract class CheckoutRepo {
 
   Future<Either<Failure, void>> makePayment(
-      {required PaymentIntentInputModel paymentIntentInputModel});
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+
+      try {
+        await StripeService().makePayment(paymentIntentInputModel: paymentIntentInputModel);
+
+        return right(null);
+
+      } on Exception catch (e) {
+        if (e is DioException) {
+          return Left(ServerFailure.fromDioException(e));
+        }
+        return Left(ServerFailure(e.toString()));
+      }
+  }
 }
