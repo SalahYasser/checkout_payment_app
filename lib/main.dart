eac066db1385d725e0421f4b6ebee874346473ef
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-07 02:25:00 +0200

    add configuration for flutter stripe

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class CheckoutApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: Colors.grey[400]),
       home: MyCartView(),
     );
   }
